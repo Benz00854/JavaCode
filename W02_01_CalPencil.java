7048e51cfa0324a24618b194b2807922641faa6a
Benz00854 <674230012@webmail.npru.ac.th>
2024-07-15 11:10:23 +0700

    Add files via upload
    First commit

---
+++ W02_01_CalPencil.java
@@ -0,0 +1,16 @@
+public class W02_01_CalPencil{
+    public static void main(String [] args){
+        // สร้างตัวแปรเพื่อเก็บค่า
+        int box;
+        int pencilPerBox;
+
+        // กำหนดค่าตัวแปร
+        box = 9;
+        pencilPerBox = 12;
+
+        System.out.print("Number of pencils = "+(box*pencilPerBox)+" แท่ง");
+        // System.out.print("Number of pencils = "+(3*12));
+        // System.out.println(3*12);
+    }
+
+}
